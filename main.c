bf9731e20c1f8143ac9a5ecba51881a20be69430
solsta <segorov555@gmail.com>
2021-10-11 20:22:01 +0100

    Added pmem part

--- main.c
+++ main.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <libpmemobj/base.h>
+#include "pmemobj_list.h"
 
-static const long buffer_size = 100000000;
+#include <glob.h>
+#include <libpmemobj.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+POBJ_LAYOUT_BEGIN(list);
+POBJ_LAYOUT_ROOT(list, struct my_root);
+POBJ_LAYOUT_END(list);
+
+#define buffer_size 100000000
+
+static PMEMobjpool *pop;
+
+struct my_root{
+    char buffer[buffer_size];
+};
+TOID(struct my_root) root;
 
 struct array{
     char *buffer;
 };
+
+static bool file_exists(char *path){
+    return access(path, F_OK) == 0;
+}
+
+
 //Sequential read
 void iterate_read(const char *buffer){
     for(int i = 0; i < buffer_size; i++ ){
@@ -34,12 +60,12 @@ void iterate_write_rand(char *buffer){
     }
 }
 
-void execute(struct array *ap, void *r(), void *w()){
+void execute(char *buffer, void *w(), void *r()){
     clock_t begin = clock();
     srand(time(NULL));
-    w(ap->buffer);
+    w(buffer);
     clock_t write_finished = clock();
-    r(ap->buffer);
+    r(buffer);
     clock_t read_finished = clock();
 
     double time_spent_writing = (double)(write_finished - begin) / CLOCKS_PER_SEC;
@@ -48,18 +74,50 @@ void execute(struct array *ap, void *r(), void *w()){
     printf("Writing: %f, Reading: %f \n", time_spent_writing, time_spent_reading);
 }
 
-int main() {
+int main(int argc, char **argv) {
     /**
      * Case 1: DRAM only.
      */
      //Initialise array on the heap:
     struct array *ap = malloc(sizeof (struct array));
     ap->buffer = malloc(sizeof (char)*buffer_size);
+    printf("DRAM results: \n");
     printf("Sequential time: ");
-    execute(ap, iterate_write, iterate_read);
+    execute(ap->buffer, iterate_write, iterate_read);
     printf("Random time: ");
-    execute(ap, iterate_write_rand, iterate_read_rand);
+    execute(ap->buffer, iterate_write_rand, iterate_read_rand);
 
+    //PMEM code
+    const char *path = argv[1];
 
+    if (file_exists(path) == false) {
+        if ((pop = pmemobj_create(path, POBJ_LAYOUT_NAME(list),
+                    sizeof(char)*buffer_size*2, 0666)) == NULL) {
+            perror("failed to create pool\n");
+            return -1;
+        }
+    } else {
+        if ((pop = pmemobj_open(path,
+                    POBJ_LAYOUT_NAME(list))) == NULL) {
+                perror("failed to open pool\n");
+                return -1;
+        }
+    }
+    printf("Pmem results: \n");
+    root = POBJ_ROOT(pop, struct my_root);
+    struct my_root *rp = D_RW(root);
+    TX_BEGIN(pop){
+        clock_t begin = clock();
+        TX_ADD_DIRECT(rp->buffer);
+        clock_t tx_range_is_added = clock();
+        double time_spent_adding_range_to_tx = (double)(tx_range_is_added - begin) / CLOCKS_PER_SEC;
+
+        printf("Time took to add a range to a transaction: %f \n", time_spent_adding_range_to_tx);
+
+        printf("Sequential time: ");
+        execute(rp->buffer, iterate_write, iterate_read);
+        printf("Random time: ");
+        execute(rp->buffer, iterate_write_rand, iterate_read_rand);
+    } TX_END
     return 0;
 }
